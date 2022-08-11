20a1f74ad1365fd190ea4bce815f3a5307369f8a
cmong0516 <cmong0516@gmail.com>
2022-08-11 21:24:10 +0900

    2022.08.11

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,3 +1,6 @@
+import domain.Member;
+import domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -12,6 +15,28 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.setTeamId(team.getId());
+            member.setTeam(team);
+            em.persist(member);
+
+            // 객체를 테이블에 맞추어 데이터 중심의 설계를 하면 협력관계를 만들수 없다.
+            // 아래처럼 각각의 Table 에서 값을 조회하여 여러번 찾아야함.
+            Member findMember = em.find(Member.class, member.getId());
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+//            String teamName = findTeam.getName();
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("findTeam = " + findTeam.getName());
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
